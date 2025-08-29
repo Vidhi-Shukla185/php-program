888af146b5806707ad7782028ad7d51b1698c33e
Vidhi-Shukla185 <vidhishukla185@gmail.com>
2025-08-29 11:53:53 -0700

    add pro12

---
+++ pro12.php
@@ -0,0 +1,20 @@
+<?php
+$a=10;
+$b=6;
+$sum= $a+$b;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP sum of two numbers</title>
+</head>
+<body>
+    <h1> PHP integer variables example</h1>
+    <p>First num:<?phpecho $a;?></p>
+     <p>Second num:<?phpecho $b;?></p>
+      <p>Sum of:<?php echo $a;?>and <?phpecho $b;?> is <?phpecho $sum;?></p>
+    
+</body>
+</html>
