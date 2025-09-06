3e93cd153f0306b9fa46816b9582323b9a262614
Vidhi-Shukla185 <vidhishukla185@gmail.com>
2025-09-05 21:30:29 -0700

    next program

--- pro16.php
+++ pro16.php
@@ -17,3 +17,31 @@ do {
     $i++;
 } while ($i <= 20);
 ?>
+=======
+// Using for loop
+echo "<h3>Numbers from 1 to 20 using For Loop:</h3>";
+for ($i = 1; $i <= 20; $i++) {
+    echo $i . " ";
+}
+
+echo "<br><br>";
+
+// Using while loop
+echo "<h3>Numbers from 1 to 20 using While Loop:</h3>";
+$i = 1;
+while ($i <= 20) {
+    echo $i . " ";
+    $i++;
+}
+
+echo "<br><br>";
+
+// Using do while loop
+echo "<h3>Numbers from 1 to 20 using Do While Loop:</h3>";
+$i = 1;
+do {
+    echo $i . " ";
+    $i++;
+} while ($i <= 20);
+?>
+>>>>>>> 1c9196c (next program)
