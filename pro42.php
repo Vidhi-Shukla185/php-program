bcc66e19eee03b301492b59294b486b02e63b892
Vidhi-Shukla185 <vidhishukla185@gmail.com>
2025-09-24 07:52:47 -0700

    pro42

---
+++ pro42.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>program 42</title>
+</head>
+<body>
+    <form action="">
+
+   enter first number: <input type="number" name="num1" id=""> <br><br>
+
+   enter second number: <input type="number" name="num2" id=""> <br><br>
+   <select name="operation" id="">
+    <option value="add"> add</option>
+     <option value="sub"> sub</option>
+      <option value="mul"> mul</option>
+       <option value="div"> div</option>
+   </select>
+   <input type="submit">
+    </form>
+</body>
+</html>
+<?php
+  $num1=$_GET['num1'];
+    $num2=$_GET['num2'];
+      $opr=$_GET['operation'];
+      switch($opr){
+      case "add":
+        $result =$num1+$num2;
+        echo $result;
+         case "sub":
+        $result =$num1-$num2;
+          echo $result;
+           case "mul":
+        $result =$num1*$num2;
+          echo $result;
+          case "div":
+        $result =$num1/$num2;
+          echo $result;
+
+
+
+
+      }
+
+
+?>
